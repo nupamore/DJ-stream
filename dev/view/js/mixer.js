c8ddb6174c7eaee49ec114738407fbc730502898
kimering <merii029@naver.com>
2016-11-24 12:55:43 +0900

    mixer 수정
    디자인 변경 & mousePressed시 멈춤

--- dev/view/js/mixer.js
+++ dev/view/js/mixer.js
@@ -1,4 +1,9 @@
 const drawMixer = () => {
+  let diskLx
+  let diskLy
+  let diskRx
+  let diskRy
+
   let speedL = 0.1
   let speedR = 0.01
   let ringL
@@ -6,13 +11,12 @@ const drawMixer = () => {
 
   let imgL
   const imgLPath= './img/user2.png'
-
   let imgR
   const imgRPath= './img/user.png'
 
 
   function setup() {
-    const canvas = createCanvas( 900, 450 )
+    const canvas = createCanvas( 900, 400 )
     canvas.parent('mixer')
 
     // 리사이징
@@ -20,6 +24,12 @@ const drawMixer = () => {
       width: '100%',
       height: 'auto'
     })
+
+    //디스크 위치 변수 초기화
+    diskLx = width*0.2
+    diskLy = height*0.5
+    diskRx = width*0.8
+    diskRy = height*0.5
 
     ringL = 0
     ringR = 0;
@@ -32,118 +42,90 @@ const drawMixer = () => {
     //draw table
     fill( 50 )
     rect( width/2, height/2, width, height )
-    //가운데 선
-    fill( 0 )
-    rect( width/2, height/2, width*0.005, height )
-
-    //draw visualizer
-    fill( 200 )
-    rect( width*0.5, height*0.1, width*0.95, height*0.12, 2, 2, 2, 2 )
 
     //디스크판
-    ellipseMode( CENTER )
-    fill( 250 )
-    ellipse( width*0.3, height*0.425, height*0.45, height*0.45 )
-    ellipse( width*0.7, height*0.425,height*0.45, height*0.45 )
+    drawDisk( diskLx, diskLy, imgL, 'left' )
+    drawDisk( diskRx, diskRy, imgR, 'right' )
 
+    //왼쪽동그라미
+    fill( 150 )
+    ellipse( width*0.43, height*0.22, height*0.15, height*0.15 )
     fill( 0 )
-    ellipse( width*0.3, height*0.425,height*0.43, height*0.43 )
-    ellipse( width*0.7, height*0.425,height*0.43, height*0.43 )
+    ellipse( width*0.43, height*0.22, height*0.13, height*0.13 )
 
-    imageMode(CENTER)
-    push()
-    translate( width*0.3, height*0.425 )
-    rotate( ringL )
-    image( imgL, 0, 0, height*0.2, height*0.2 )
-    fill(0, 0, 0, 0)
-    stroke(0)
-    strokeWeight( height*0.1 )
-    ellipse( 0, 0, height*0.3, height*0.3 )
-    ringL += speedL
-    pop()
-    push()
-    translate( width*0.7, height*0.425 )
-    rotate( ringR )
-    image( imgR, 0, 0, height*0.2, height*0.2 )
-    fill(0, 0, 0, 0)
-    stroke(0)
-    strokeWeight( height*0.1 )
-    ellipse( 0, 0, height*0.3, height*0.3 )
-    ringR += speedR
-    pop()
+    //오른쪽동그라미
+    fill( 150 )
+    ellipse( width*0.57, height*0.22, height*0.15, height*0.15 )
+    fill( 0 )
+    ellipse( width*0.57, height*0.22, height*0.13, height*0.13 )
 
-    //컨트롤러
-    //왼쪽큰거
-    fill( 80 )
-    rect( width*0.075, height*0.45, width*0.1, height*0.5 )
-    fill( 255 )
-    rect( width*0.075, height*0.4, width*0.008, height*0.33 )
-    rect( width*0.075, height*0.63, width*0.07, height*0.08 )
-    fill( 20, 255, 20 )
-
-    //오른쪽큰거
-    fill( 80 )
-    rect( width*0.925, height*0.45, width*0.1, height*0.5 )
-    fill( 255 )
-    rect( width*0.925, height*0.4, width*0.008, height*0.33 )
-    rect( width*0.925, height*0.63, width*0.07, height*0.08 )
+    //왼쪽 이퀄라이저
+    fill( 150 )
+    rect( width*0.43, height*0.53, width*0.042, height*0.35 )
+    fill( 0 )
+    rect( width*0.43, height*0.53, width*0.035, height*0.33 )
+    //오른쪽 이퀄라이저
+    fill( 150 )
+    rect( width*0.57, height*0.53, width*0.042, height*0.35 )
+    fill( 0 )
+    rect( width*0.57, height*0.53, width*0.035, height*0.33 )
 
     //가운데큰거
-    fill( 30 )
+    fill( 0 )
     rect( width*0.5, height*0.85, width*0.2, height*0.2 )
-
-    //left
-    fill( 250 )
-    rect( width*0.05, height*0.85, width*0.005, height*0.18 )
-    rect( width*0.12, height*0.85, width*0.005, height*0.18 )
-    rect( width*0.19, height*0.85, width*0.005, height*0.18 )
-    rect( width*0.3, height*0.85, width*0.005, height*0.18 )
-
-    //right
-    rect( width*0.95, height*0.85, width*0.005, height*0.18 )
-    rect( width*0.88, height*0.85, width*0.005, height*0.18 )
-    rect( width*0.81, height*0.85, width*0.005, height*0.18 )
-    rect( width*0.7, height*0.85, width*0.005, height*0.18 )
-
-    if( mouseIsPressed ) {
-      rect(width/2, height/2, 100, 100)
-    }
+    fill( 150 )
+    rect( width*0.5, height*0.85, width*0.17, height*0.01)
+    fill( 255 )
+    rect( width*0.5, height*0.85, width*0.025, height*0.1 )
   }
 
-  /**
-   * 오른쪽디스크 빙글빙글
-   * @param  {[type]} x [L디스크중심x좌표]
-   * @param  {[type]} y [L디스크중심y좌표]
-   * @return none
-   */
-  function angelRingL( x, y ) {
-    push()
-    translate( x, y )
-    stroke( 255 )
-    strokeWeight( 3 )
-    rotate( ringL )
-    line( 0, 0, 0, height * 0.18 )
-
-    ringL += speedL
-    pop()
+/**
+ * 디스크를 그립니다
+ * @param  {[float]} x        [디스크 중심 x위치]
+ * @param  {[float]} y        [디스크 중심 y 위치]
+ * @param  {[path]} img      [이미지]
+ * @param  {[string]} position [오른쪽/왼쪽 구분]
+ * @return {[none]}          [리턴 안합니다]
+ */
+  function drawDisk( x, y, img, position ) {
+    ellipseMode( CENTER )
+    imageMode(CENTER)
+    noStroke()
+      push()
+      translate( x, y )
+      fill( 250 )
+      ellipse( 0, 0, height*0.62, height*0.62 )
+      fill( 0 )
+      ellipse( 0, 0, height*0.6, height*0.6 )
+      fill(0, 0, 0, 0)
+      stroke(0)
+      strokeWeight( height*0.1 )
+      if( position == 'left') {
+        push()
+        rotate( ringL )
+        image( imgL, 0, 0, height*0.3, height*0.3 )
+        ellipse( 0, 0, height*0.4, height*0.4 )
+        if( !( mouseIsPressed )) {
+          ringL += speedL
+        }
+        pop()
+      }
+      else if( position == 'right' ) {
+        push()
+        rotate( ringR )
+        image( imgR, 0, 0, height*0.3, height*0.3 )
+        ellipse( 0, 0, height*0.4, height*0.4 )
+        if( !mouseIsPressed ) {
+            ringR += speedR
+          }
+        }
+        else {
+        }
+        pop()
+      }
+      pop()
   }
 
-  /**
-   * 오른쪽디스크빙글빙글
-   * @param  {[type]} x [R디스크중심x좌표]
-   * @param  {[type]} y [R디스크중심y좌표]
-   */
-  function angelRingR( x, y ) {
-    push()
-    translate( x, y )
-    stroke( 255 )
-    strokeWeight( 3 )
-    rotate( ringR )
-    line( 0, 0, 0, height * 0.18 )
-
-    ringR += speedR
-    pop()
-  }
 
 
   function preload() {
