310e08f49c9de3b61b358850c52a2b47958b26ca
kimering <merii029@naver.com>
2016-11-27 19:34:38 +0900

    search정상화, user 하나만

--- dev/router/user.js
+++ dev/router/user.js
@@ -13,33 +13,21 @@ const connectionInfo = {
 }
 
 const query = {
-  searchByUserId : `
+  userId : `
     SELECT USER_NICKNAME, USER_IMG, USER_TYPE
     FROM USER
     WHERE USER_ID = ?
-    ;`,
-  searchByWaveName : `
-    SELECT WAVE_DJ, WAVE_NAME, WAVE_DESC, WAVE_LIVE, WAVE_IMG, WAVE_VIEW, WAVE_DT
-    FROM WAVE
-    WHERE WAVE_DJ = ? AND WAVE_NAME = ?
     ;`
 }
 
 router.get( '/:userId', (req, res) => {
   const id = req.query.userId
   const connection = mysql.createConnection( connectionInfo )
-  connection.query( query.searchByUserId, ['userid'], ( err, rows, fields ) => {
-
+  connection.query( query.userId, [id], ( err, rows, fields ) => {
+    if( err ) throw err
+    res.json( rows )
   })
 })
 
-router.get( '/:userId/:waveName', (req, res) => {
-  const id = req.query.userId
-  const waveName = req.query.waveName
-  const connection = mysql.createConnection ( connectionInfo )
-  connection.query( query.searchByWaveName, ['userid', 'waveName'],( err, rows, fields ) => {
-
-  })
-})
 
 module.exports = router
