354215bdfa41a56d1230275d7f1cc34d4a55e2cd
kimering <merii029@naver.com>
2016-11-28 15:01:13 +0900

    후원팔로우

--- dev/router/support.js
+++ dev/router/support.js
@@ -1,12 +1,40 @@
 
 // modules
 const express = require('express')
+const bodyParser = require('body-parser');
+const express = require('mysql')
 
-
+const jsonParser = bodyParser.json({
+  type : 'application/*+json'
+})
 const router = express.Router()
 
-router.get( '/support', (req, res) => {
-  res.send( 'test' )
+const connectionInfo = {
+  host : 'nupa.fun25.co.kr',
+  port : 17904,
+  user : 'hyerim',
+  password : 'rimhye',
+  database : 'djstream'
+}
+
+const query = {
+  support : `
+    INSERT INTO SUPPORT ( USER_ID, SUPPORT_DJ, SUPPORT_DT )
+    VALUES ( ?, ?, NOW() );`
+}
+
+router.get( '/support', jsonParser, (req, res) => {
+  const user = req.body.me
+  const dj = req.body.dj
+  const connection = mysql.createConnection( connectionInfo )
+  connection.query( query.follow, [ user, dj ], ( err, result ) => {
+      if( err ) {
+        res.sendStatus( 404 )
+      }
+      else {
+        res.json( result )
+      }
+  })
 })
 
 module.exports = router
