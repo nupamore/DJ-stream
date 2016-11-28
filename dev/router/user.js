210e386ac5cb4d440e67b9b7aeea8a3c60816615
kimering <merii029@naver.com>
2016-11-28 14:19:47 +0900

    유저를 볼수 있습니다

--- dev/router/user.js
+++ dev/router/user.js
@@ -2,6 +2,8 @@
 // modules
 const express = require('express')
 const mysql = require('mysql')
+const async = require('async')
+
 
 const router = express.Router()
 const connectionInfo = {
@@ -32,72 +34,70 @@ const query = {
           USER.USER_ID = ?  `,
   //내 작품 조회
   myWave : `
-    SELECT WAVE.WAVE_ID id, WAVE.WAVE_DJ dj. WAVE.WAVE_NAME name, WAVE.WAVE_DESC desc, WAVE.WAVE_IMG img, WAVE.WAVE_DT dt, WAVE.WAVE_VIEW view
+    SELECT WAVE.WAVE_ID id, WAVE.WAVE_DJ dj, WAVE.WAVE_NAME name, WAVE.WAVE_DESC de, WAVE.WAVE_IMG img, WAVE.WAVE_DT dt, WAVE.WAVE_VIEW view
     FROM USER, WAVE
-    WHERE USER.USER_ID = WAVE.WAVE_DJ AND WAVE.WAVE_DJ = ?; `
+    WHERE USER.USER_ID = WAVE.WAVE_DJ AND
+          WAVE.WAVE_DJ = ?; `
 }
 
 
 
 router.get( '/:userId', (req, res) => {
-  const id = req.query.userId
+  const id = req.params.userId
   const connection = mysql.createConnection( connectionInfo )
-  const user = []
-  connection.query( query.myInfo, [id], ( err, rows, fields ) => {
-    if( err ) throw err
-<<<<<<< HEAD
-    user = rows.map( x => {
-      return {
-        name : x.USER_NICKNAME,
-        img : x.USER_IMG,
-        dt: x.USER_DT,
-      }
-    })
-=======
 
-    // 예시입니다!
-    const user = {
-      id: 'hyerim',
-      name: '혜림',
-      img: '/img/user.png',
-      dt: '2016-11-27',
-      following: [
-        {
-          name: 'sangbaek'
-        }
-      ],
-      follower: [
-        {
-          name: 'sangbaek'
-        },
-        {
-          name: 'jonghoon'
-        }
-      ],
-      waves: [
-        {
-          id: 5,
-          dj: 'hyerim',
-          name: 'yo',
-          desc: 'check it out',
-          img: '/img/logo.png',
-          view: 123,
-          dt: '2016-11-27',
-        },
-        {
-          id: 6,
-          dj: 'hyerim',
-          name: 'this',
-          desc: 'is a pen',
-          img: '/img/logo.png',
-          view: 123,
-          dt: '2016-11-27',
-        }
-      ]
+  async.parallel([
+    callback => {
+      connection.query( query.myInfo, [id], ( err, rows, fields ) => {
+        callback( err, rows )
+      })
+    },
+    callback => {
+      connection.query( query.myFollower, [id], ( err, rows, fields ) => {
+        callback( err, rows )
+      })
+    },
+    callback => {
+      connection.query( query.myFollowing, [id], ( err, rows, fields ) => {
+        callback( err, rows )
+      })
+    },
+    callback => {
+      connection.query( query.myWave, [id], ( err, rows, fields ) => {
+        callback( err, rows )
+      })
     }
->>>>>>> refs/remotes/origin/master
+  ], ( err, results ) => {
+    const info = results[0][0]
+    const follower = results[1]
+    const following = results[2]
+    const wave = results[3]
+
+    console.log(info)
+    const user = {
+      id : info.USER_ID,
+      name : info.USER_NICKNAME,
+      img : info.USER_IMG,
+      dt : info.USER_DT,
+      follower : follower.map( x => ({
+        name : x.myfollower
+      })),
+      following : following.map( x => ({
+        name : x.myfollowing
+      })),
+      waves : wave.map( x => ({
+        id : x.id,
+        dj : x.dj,
+        name : x.name,
+        desc : x.desc,
+        img : x.img,
+        view : x.view,
+        dt : x.dt
+      }))
+    }
     res.json( user )
   })
+
 })
 
 
