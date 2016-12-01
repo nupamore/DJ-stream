c5c4fa8437c47fc777e8da04ae54569c4556c60b
Sangbaek Lee <nupamore@gmail.com>
2016-12-01 16:37:40 +0900

    리팩토링
    connectionInfo 수정
    data.sql 이동
    404페이지 추가

--- dev/router/search.js
+++ dev/router/search.js
@@ -1,15 +1,9 @@
 // modules
 const express = require('express')
 const mysql = require('mysql')
+const db = require('../custom_modules/db.js')
 
 const router = express.Router()
-const connectionInfo = {
-  host : 'nupa.fun25.co.kr',
-  port : 17904,
-  user : 'hyerim',
-  password : 'rimhye',
-  database : 'djstream'
-}
 
 const query = {
   search : `
@@ -22,7 +16,7 @@ const query = {
 //작품 검색
 router.get( '/search', (req, res) => {
   const keyword = req.query.k
-  const connection = mysql.createConnection( connectionInfo )
+  const connection = mysql.createConnection( db.connectionInfo )
   connection.query( query.search, [`%${ keyword }%`, `%${ keyword }%`], ( err, rows, fields ) => {
       if( err ) throw err
 
