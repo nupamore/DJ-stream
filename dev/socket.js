76c8c46b081586369a1007fe5c656b11879de042
kimering <merii029@naver.com>
2016-12-04 05:07:07 +0900

    소켓 채팅 구현
    UI 이상해여ㅎㅅㅎ 고쳐줭

--- dev/socket.js
+++ dev/socket.js
@@ -6,15 +6,28 @@ const socket = require('socket.io')
 exports.listen = ( http ) => {
   const io = socket.listen( http )
 
+  var userNames = []
+
   io.on( 'connection', ( socket ) => {
-    console.log( 'user connected' )
+    socket.on( 'addUser', ( userName ) => {
+      socket.userName = userName
+      socket.room = 'waveName'
+      userNames.push(userName)
+      socket.join( socket.room )
+      io.sockets.in( socket.room ).emit( 'chat', 'SERVER', socket.userName + '님이 입장하셨습니다. ')
+      socket.emit( 'updateUser', userNames )
+    })
+
+    socket.on( 'sendChat', ( comment ) => {
+      io.sockets.in( socket.room ).emit( 'chat', socket.userName, comment )
+    })
 
     socket.on( 'disconnect', () => {
-      console.log( 'user disconnected' )
+      userNames.splice( userNames.indexOf(socket.userName), 1 )
+      io.sockets.emit( 'updateuser', userNames )
+      socket.broadcast.in( socket.waveName ).emit( 'chat', 'SERVER', socket.userName + '님이 퇴장하셨습니다. ' )
+      socket.leave( socket.room )
     })
 
-    socket.on( 'adduser', ( userName ) => {
-      console.log( userName )
-    })
   })
 }
--- dev/view/js/client.js
+++ dev/view/js/client.js
@@ -1,9 +1,33 @@
 
 const socketClient = ( waveName ) => {
-var socket = io.connect('http://localhost:65007');
-    // 서버에 접속할 때, 사용자명을 확인한다.
-    socket.on('connect', function() {
-        // 서버에 있는 adduser 함수를 호출하며, 하나의 파라미터(prompt의 반환 값)를 전달한다
-        socket.emit('adduser', app.me.name );
-    });
+var socket = io.connect('http://localhost:65007')
+  socket.on( 'connect', () =>  {
+    socket.emit('addUser', prompt( '이름을 입력해주세염 ' ) );
+  });
+
+  socket.on( 'updateUser', ( userNames ) => {
+    $('#users').empty();
+    console.log(userNames)
+    $.each( userNames, ( key, value ) => {
+      $('#users').append(`${ value } <br>`)
+    })
+  })
+
+  socket.on( 'chat', ( userName, comment ) => {
+    $('#chat').append( `<b>${ userName }</b> : ${ comment } <br>` )
+  })
+
+  $( function() {
+    $('#datasend').click( () => {
+      const message = $('#data').val()
+      $('#data').val('')
+      socket.emit( 'sendChat', message )
+    })
+    $('#data').keypress( ( e ) => {
+      if( e.which == 13 ){
+        $(this).blur()
+        $('#datasend').focus().click()
+      }
+    })
+  })
 }
